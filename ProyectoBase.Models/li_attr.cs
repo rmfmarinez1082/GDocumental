35b378ea5a453172dda3bb6b13004e981a3c8afb
Luis Antonio <luisantonio961@hotmail.com>
2022-11-25 01:02:31 -0600

    clasesJS

---
+++ ProyectoBase.Models/li_attr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBase.Models
+{
+    public class li_attr
+    {
+        public string id { get; set; }
+        public string datajstree { get; set; }
+    }
+}
