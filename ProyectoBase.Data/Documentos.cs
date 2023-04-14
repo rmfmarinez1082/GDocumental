e5579e4fa6f839b132065b519884814510b7090d
Luis Antonio <luisantonio961@hotmail.com>
2023-04-14 11:51:46 -0600

    Formato Fechas

--- ProyectoBase.Data/Documentos.cs
+++ ProyectoBase.Data/Documentos.cs
@@ -251,9 +251,7 @@ namespace ProyectoBase.Data
             b.AddParameter("@IdTipoArchivo", Adoc.IdTipoArchivo, SqlDbType.Int);
             b.AddParameter("@IdMedioAlmacenamiento", Adoc.IdMedioAlmacenamiento, SqlDbType.Int);
             b.AddParameter("@IdClasificacion", Adoc.IdClasificacion, SqlDbType.Int);
-            //b.AddParameter("@IdClasificacionArchivo", Adoc.IdClasificacionArchivo, SqlDbType.Int);
-            //b.AddParameter("@IdSubclasificacionArchivo", Adoc.IdSubclasificacionArchivo, SqlDbType.Int);
-            //b.AddParameter("@IdNombre3  ", Adoc.IdNombre3, SqlDbType.Int);
+            
             Models.Documento resultado = new Models.Documento();
             var reader = b.ExecuteReader();
             while (reader.Read())
@@ -355,13 +353,11 @@ namespace ProyectoBase.Data
 
         {
             b.ExecuteCommandSP("SP_ActualizarDoc");
-            //b.AddParameter("@Nombre", nuevoDocumento.Nombre, SqlDbType.NVarChar);
             b.AddParameter("@Id", nuevoDocumento.Id, SqlDbType.Int);
             b.AddParameter("@IdUser", nuevoDocumento.IdUsuario, SqlDbType.Int);
             b.AddParameter("@NmArchivo", nuevoDocumento.NmArchivo, SqlDbType.NVarChar);
             b.AddParameter("@NMArchivoEditable", nuevoDocumento.NmArchivoword, SqlDbType.NVarChar);
             b.AddParameter("@Version", nuevoDocumento.Version, SqlDbType.NVarChar);
-            //b.AddParameter("@NmOriginal", nuevoDocumento.NmOriginal, SqlDbType.NVarChar);
            
 
 
